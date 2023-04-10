7cf802c014cd97afe09799c1bf8132c7cdd8ece7
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2023-04-10 15:03:31 +0200

    CSS oki

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import Header from "./Header.svelte";
   import Lerp from "../components/lerp/Lerp.svelte";
 </script>
+<style lang="scss" global>
+  @import '../css/main';
+</style>
+
   <Lerp />
   <Header />
 
