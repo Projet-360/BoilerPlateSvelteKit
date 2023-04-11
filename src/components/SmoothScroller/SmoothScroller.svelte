31004cd423884d83757af1dce4fe0da5b2581d18
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2023-04-12 00:51:03 +0200

    smooth scroller

--- src/components/SmoothScroller/SmoothScroller.svelte
+++ src/components/SmoothScroller/SmoothScroller.svelte
@@ -1,37 +1,45 @@
-<style>
-    .scroll-container {
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      overflow: hidden;
-    }
-  
-    .scroll-content {
-      position: relative;
-      transform: translateY(0);
-      transition: transform 0.5s ease;
-      height: 300vh;
-    }
-  </style>
-  
-  <div class="scroll-container">
-    <div class="scroll-content">
-      <slot/>
-    </div>
-  </div>
-  
-  <script>
-    const container = document.querySelector('.scroll-container');
-    const content = document.querySelector('.scroll-content');
-    let scrollPos = 0;
-  
-    container.addEventListener('scroll', (event) => {
-      const newScrollPos = container.scrollTop;
-      const delta = newScrollPos - scrollPos;
-      content.style.transform = `translateY(${-newScrollPos}px)`;
-      scrollPos = newScrollPos;
+<script>
+  import { writable } from "svelte/store";
+
+  let smoothscroller;
+  const scrollY = writable(0);
+  const scrollSpeed = 1; // Modifier cette valeur pour ajuster la vitesse de défilement
+
+  function handleScroll(event) {
+    scrollY.update((value) => {
+      const newPosition = value - event.deltaY * scrollSpeed;
+      const maxScroll = smoothscroller.scrollHeight - smoothscroller.clientHeight;
+
+      if (newPosition < 0) {
+        event.preventDefault(); // Empêche le défilement natif du navigateur
+        return 0; // Bloque le défilement en haut
+      } else if (newPosition > maxScroll) {
+        return maxScroll; // Bloque le défilement en bas
+      } else {
+        return newPosition;
+      }
     });
-  </script>
-  
+  }
+</script>
+
+<style>
+  .smoothscroller {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow-y: scroll;
+  }
+
+  .smoothscroller-wrapper {
+    will-change: transform;
+    transition: transform 0.5s; /* Modifier la durée de la transition pour ajuster la vitesse */
+  }
+</style>
+
+<div class="smoothscroller" bind:this={smoothscroller} on:wheel={handleScroll}>
+  <div class="smoothscroller-wrapper" style="transform: translateY({$scrollY}px)">
+    <slot/>
+  </div>
+</div>
