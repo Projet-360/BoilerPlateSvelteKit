e7b3e760463f968b7725f5014ba982f0b73cf386
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2023-09-13 12:08:55 +0200

    translate

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	  import { t } from '$UITools/translations/index.js';
+
+</script>
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="About this app" />
@@ -6,4 +10,6 @@
 <div>
 	<h1>About</h1>
 	<a href="/">Lien vers la page home</a>
+	<h1>{$t('about.title')}</h1>
+	<p>{@html $t('about.text')}</p>
 </div>
