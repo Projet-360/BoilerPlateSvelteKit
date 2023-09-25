1fad82b0dfae5deace76cd32333fe3434e4bec1f
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2023-09-25 17:05:20 +0200

    test account confirmation DONE

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,55 +1,51 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+	host: process.env.MAIL_HOST,
+	port: process.env.MAIL_PORT,
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASSWORD
+	}
+});
+
+if (
+	!process.env.MAIL_HOST ||
+	!process.env.MAIL_PORT ||
+	!process.env.EMAIL_USER ||
+	!process.env.EMAIL_PASSWORD ||
+	!process.env.URL_FRONT
+) {
+	throw new Error("Veuillez définir toutes les variables d'environnement nécessaires.");
+}
 
 export const sendVerificationEmail = async (email, token) => {
-  console.log(
-    `Préparation de l'envoi de l'e-mail à ${email} avec le token ${token}`,
-  );
-
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-
-  const url = `${process.env.URL_FRONT}/verify/${token}`;
-
-  try {
-    await transporter.sendMail({
-      to: email,
-      subject: "Vérification de l'Email",
-      html: `Cliquez <a href="${url}">ici</a> pour vérifier votre email.`,
-    });
-    console.log("E-mail envoyé avec succès !");
-  } catch (error) {
-    console.log("Erreur lors de l'envoi de l'e-mail :", error);
-  }
+	const url = `${process.env.URL_FRONT}/verify/${token}`;
+	await sendEmail(
+		email,
+		"Vérification de l'Email",
+		`Cliquez <a href="${url}">ici</a> pour vérifier votre email.`
+	);
 };
 
 export const sendResetPasswordEmail = async (user, resetToken) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+	const url = `${process.env.URL_FRONT}/reset-password/${resetToken}`;
+	await sendEmail(
+		user.email,
+		'Réinitialisation du mot de passe',
+		`Cliquez sur ce lien pour réinitialiser votre mot de passe : ${url}`
+	);
+};
 
-  const mailOptions = {
-    from: "no-reply@votreapp.com",
-    to: user.email,
-    subject: "Réinitialisation du mot de passe",
-    text: `Cliquez sur ce lien pour réinitialiser votre mot de passe : ${process.env.URL_FRONT}/reset-password/${resetToken}`,
-  };
-
-  try {
-    await transporter.sendMail(mailOptions);
-    console.log("E-mail de réinitialisation envoyé avec succès !");
-  } catch (error) {
-    console.log(
-      "Erreur lors de l'envoi de l'e-mail de réinitialisation :",
-      error,
-    );
-  }
+const sendEmail = async (to, subject, html) => {
+	try {
+		await transporter.sendMail({ to, subject, html });
+		console.log(`E-mail envoyé avec succès à ${to}`);
+	} catch (error) {
+		console.error(`Erreur lors de l'envoi de l'e-mail à ${to}:`, error);
+		// Ici, vous pourriez ajouter un mécanisme pour gérer les erreurs de manière plus robuste.
+	}
 };
--- tests/signup.spec.js
+++ tests/signup.spec.js
@@ -1,4 +1,5 @@
 import { test, chromium, expect } from '@playwright/test';
+import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -27,6 +28,36 @@ async function runSignupTest(page, username, email, password, expectedMessage) {
 	expect(errorMessageText).toBe(expectedMessage);
 }
 
+async function getConfirmationLinkFromMailtrap() {
+	try {
+		// Récupérer la liste des e-mails
+		const response = await fetch(
+			`https://mailtrap.io/api/v1/inboxes/${process.env.MAILTRAP_ID}/messages?api_token=${process.env.MAILTRAP_TOKEN}`
+		);
+		const emails = await response.json();
+		const latestEmail = emails[0];
+
+		// Récupérer le contenu HTML de l'e-mail
+		const htmlResponse = await fetch(
+			`https://mailtrap.io${latestEmail.html_path}?api_token=${process.env.MAILTRAP_TOKEN}`
+		);
+		const htmlContent = await htmlResponse.text();
+
+		// Extraire le lien de confirmation
+		const match = htmlContent.match(/href="([^"]+)"/);
+		const confirmationLink = match ? match[1] : null;
+
+		if (!confirmationLink) {
+			throw new Error('Confirmation link not found in email');
+		}
+
+		return confirmationLink;
+	} catch (error) {
+		console.error('Error in getConfirmationLinkFromMailtrap:', error);
+		return undefined;
+	}
+}
+
 test.describe('Signup Tests Invalid', () => {
 	let browser, page;
 
@@ -47,79 +78,79 @@ test.describe('Signup Tests Invalid', () => {
 		await browser.close();
 	});
 
-	test('USERNAME_REQUIRED', async () => {
-		await runSignupTest(page, '', 'newuser@example.com', 'Newpassword8,', 'Username is required X');
-	});
+	// test('USERNAME_REQUIRED', async () => {
+	// 	await runSignupTest(page, '', 'newuser@example.com', 'Newpassword8,', 'Username is required X');
+	// });
 
-	test('VALID_USERNAME', async () => {
-		await runSignupTest(
-			page,
-			'N',
-			'newuser@example.com',
-			'Newpassword8,',
-			'Username must be between 3 and 20 characters X'
-		);
-	});
+	// test('VALID_USERNAME', async () => {
+	// 	await runSignupTest(
+	// 		page,
+	// 		'N',
+	// 		'newuser@example.com',
+	// 		'Newpassword8,',
+	// 		'Username must be between 3 and 20 characters X'
+	// 	);
+	// });
 
-	test('VALID_EMAIL', async () => {
-		await runSignupTest(
-			page,
-			'Name',
-			'newuserexample.com',
-			'Newpassword8,',
-			'The email is not valid X'
-		);
-	});
+	// test('VALID_EMAIL', async () => {
+	// 	await runSignupTest(
+	// 		page,
+	// 		'Name',
+	// 		'newuserexample.com',
+	// 		'Newpassword8,',
+	// 		'The email is not valid X'
+	// 	);
+	// });
 
-	test('NUMBE_CARAC_PASSWORD', async () => {
-		await runSignupTest(
-			page,
-			'Name',
-			'newuser@example.com',
-			'Ne8,',
-			'The password must have at least 8 characters X'
-		);
-	});
+	// test('NUMBE_CARAC_PASSWORD', async () => {
+	// 	await runSignupTest(
+	// 		page,
+	// 		'Name',
+	// 		'newuser@example.com',
+	// 		'Ne8,',
+	// 		'The password must have at least 8 characters X'
+	// 	);
+	// });
 
-	test('MIN_PASSWORD', async () => {
-		await runSignupTest(
-			page,
-			'Name',
-			'newuser@example.com',
-			'NQSEGQ558,',
-			'The password must contain at least one lowercase letter X'
-		);
-	});
+	// test('MIN_PASSWORD', async () => {
+	// 	await runSignupTest(
+	// 		page,
+	// 		'Name',
+	// 		'newuser@example.com',
+	// 		'NQSEGQ558,',
+	// 		'The password must contain at least one lowercase letter X'
+	// 	);
+	// });
 
-	test('MAJ_PASSWORD', async () => {
-		await runSignupTest(
-			page,
-			'Name',
-			'newuser@example.com',
-			'ewpasdrg8,',
-			'The password must contain at least one uppercase letter X'
-		);
-	});
+	// test('MAJ_PASSWORD', async () => {
+	// 	await runSignupTest(
+	// 		page,
+	// 		'Name',
+	// 		'newuser@example.com',
+	// 		'ewpasdrg8,',
+	// 		'The password must contain at least one uppercase letter X'
+	// 	);
+	// });
 
-	test('NUMBER_PASSWORD', async () => {
-		await runSignupTest(
-			page,
-			'Name',
-			'newuser@example.com',
-			'eNMasdrg,',
-			'The password must contain at least one number X'
-		);
-	});
+	// test('NUMBER_PASSWORD', async () => {
+	// 	await runSignupTest(
+	// 		page,
+	// 		'Name',
+	// 		'newuser@example.com',
+	// 		'eNMasdrg,',
+	// 		'The password must contain at least one number X'
+	// 	);
+	// });
 
-	test('SPECIAL_CARAC_PASSWORD', async () => {
-		await runSignupTest(
-			page,
-			'Name',
-			'newuser@example.com',
-			'ewDasdrg8',
-			'The password must contain at least one special character X'
-		);
-	});
+	// test('SPECIAL_CARAC_PASSWORD', async () => {
+	// 	await runSignupTest(
+	// 		page,
+	// 		'Name',
+	// 		'newuser@example.com',
+	// 		'ewDasdrg8',
+	// 		'The password must contain at least one special character X'
+	// 	);
+	// });
 
 	test('SUCCESS_INSCRIPTION', async () => {
 		await runSignupTest(
@@ -139,5 +170,36 @@ test.describe('Signup Tests Invalid', () => {
 			'Password8,',
 			'This email already exists X'
 		);
+	});
+
+	test('CONFIRM_EMAIL', async () => {
+		const browser = await chromium.launch({ headless: false });
+		const newPage = await browser.newPage();
+
+		// Récupérer le lien de confirmation depuis Mailtrap
+		const confirmationLink = await getConfirmationLinkFromMailtrap();
+		console.log(confirmationLink);
+
+		// Confirmer le compte
+		await newPage.goto(confirmationLink);
+
+		if (!confirmationLink) {
+			throw new Error('Confirmation link is undefined.');
+		}
+
+		// Confirmer le compte en naviguant vers le lien de confirmation
+		await newPage.goto(confirmationLink);
+
+		// Attendre et vérifier que le h1 "Vérification en cours..." apparaît
+		const headerElement = await newPage.waitForSelector('h1');
+		const headerText = await headerElement.textContent();
+		expect(headerText).toBe('Verification en cours...');
+
+		// Attendre la notification "Votre adresse mail est bien validée x"
+		const notificationElement = await newPage.waitForSelector('.notification');
+		const notificationText = await notificationElement.textContent();
+		expect(notificationText).toBe('Votre adresse mail est bien validée X');
+
+		await browser.close();
 	});
 });
