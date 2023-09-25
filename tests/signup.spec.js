14e1f3973e55ce81de421c57e34932a2cacf02ba
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2023-09-25 14:58:53 +0200

    test Signup input DONE

--- tests/signup.spec.js
+++ tests/signup.spec.js
@@ -209,3 +209,73 @@ test('MAJ_PASSWORD', async () => {
 
 	await browser.close();
 });
+
+test('NUMBER_PASSWORD', async () => {
+	const browser = await chromium.launch({ headless: false });
+	const newPage = await browser.newPage();
+
+	// Navigation
+	await newPage.goto('http://localhost:5173/signup', { waitUntil: 'networkidle' });
+
+	// Username
+	await newPage.focus('#username');
+	await newPage.keyboard.type('Name');
+	await newPage.waitForTimeout(200);
+
+	// Email
+	await newPage.focus('#email');
+	await newPage.keyboard.type('newuser@example.com');
+	await newPage.waitForTimeout(200);
+
+	// Password
+	await newPage.focus('#password');
+	await newPage.keyboard.type('eNMasdrg,');
+	await newPage.waitForTimeout(200);
+
+	// Submit
+	await newPage.click('button[type="submit"]');
+
+	// Wait for error message
+	const errorMessageElement = await newPage.waitForSelector('.notification');
+
+	// Verify error message text
+	const errorMessageText = await errorMessageElement.textContent();
+	expect(errorMessageText).toBe('The password must contain at least one number X');
+
+	await browser.close();
+});
+
+test('SPECIAL_CARAC_PASSWORD', async () => {
+	const browser = await chromium.launch({ headless: false });
+	const newPage = await browser.newPage();
+
+	// Navigation
+	await newPage.goto('http://localhost:5173/signup', { waitUntil: 'networkidle' });
+
+	// Username
+	await newPage.focus('#username');
+	await newPage.keyboard.type('Name');
+	await newPage.waitForTimeout(200);
+
+	// Email
+	await newPage.focus('#email');
+	await newPage.keyboard.type('newuser@example.com');
+	await newPage.waitForTimeout(200);
+
+	// Password
+	await newPage.focus('#password');
+	await newPage.keyboard.type('ewDasdrg8');
+	await newPage.waitForTimeout(200);
+
+	// Submit
+	await newPage.click('button[type="submit"]');
+
+	// Wait for error message
+	const errorMessageElement = await newPage.waitForSelector('.notification');
+
+	// Verify error message text
+	const errorMessageText = await errorMessageElement.textContent();
+	expect(errorMessageText).toBe('The password must contain at least one special character X');
+
+	await browser.close();
+});
