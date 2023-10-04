82dcb6ea20a87438e6278a7d1d733b38b24a6877
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2023-10-05 01:46:42 +0200

    command clear DB

---
+++ backend/clear-db.js
@@ -0,0 +1,23 @@
+import { MongoClient } from 'mongodb';
+import { env } from './constants/env.js';
+
+async function clearDB() {
+	const client = new MongoClient(env.BD);
+
+	try {
+		await client.connect();
+		const db = client.db(env.DB_NAME);
+		const collections = await db.collections();
+
+		for (const collection of collections) {
+			await collection.deleteMany({});
+		}
+		console.log('Database cleared');
+	} catch (err) {
+		console.error(err);
+	} finally {
+		await client.close();
+	}
+}
+
+clearDB();
--- backend/constants/env.js
+++ backend/constants/env.js
@@ -1,5 +1,6 @@
 export const env = {
 	BD: 'mongodb://localhost:27027/svelteKitDB',
+	DB_NAME: 'svelteKitDB',
 	TOKEN_EXPIRY: '1h',
 	URL_FRONT: 'http://localhost:5173',
 	URL_BACK: 'http://localhost:3001',
