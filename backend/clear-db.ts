f6b070304fde2de356c29ea7b48a2b0e1a46a821
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2023-10-22 16:14:38 +0200

    clear db default users

--- backend/clear-db.ts
+++ backend/clear-db.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import { env } from './constants/env.js';
 import logger from './services/logger.js';
 
@@ -16,6 +16,36 @@ async function clearDB() {
 
     // Utilisation de Winston pour le logging
     logger.info('Database cleared');
+
+    // INSERTION DES DONNÉES DE BASE POUR LES UTILISATEURS
+
+    const usersCollection = db.collection('users');
+    await usersCollection.insertMany([
+      {
+        _id: new ObjectId('6535292976d3db78fbc95f3e'),
+        username: 'Admin',
+        email: 'admin@admin.fr',
+        password:
+          '$2a$12$Rd43bsIzTi3Dlwq7rgNCWuRHqOap82AdMCSSbFxYjqWmhG0hfBp/6', // Aaaaaaa8,
+        isVerified: true,
+        role: 'admin',
+        __v: 0,
+      },
+      {
+        _id: new ObjectId('65352a1b76d3db78fbc95f46'),
+        username: 'User',
+        email: 'user@user.fr',
+        password:
+          '$2a$12$Rd43bsIzTi3Dlwq7rgNCWuRHqOap82AdMCSSbFxYjqWmhG0hfBp/6',
+        isVerified: true,
+        role: 'user',
+        __v: 0,
+      },
+      // ... autres utilisateurs
+    ]);
+
+    // Utilisation de Winston pour le logging
+    logger.info('Default users inserted');
   } catch (err) {
     // Utilisation de Winston pour le logging d'erreur
     logger.error(err);
