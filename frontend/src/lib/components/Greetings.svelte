35a43289498e2fe619f4f10938eab6ac707f8994
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-04-18 22:27:23 +0200

    wip integration graph frontend

--- frontend/src/lib/components/Greetings.svelte
+++ frontend/src/lib/components/Greetings.svelte
@@ -50,3 +50,46 @@
 		</button>
 	</li>
 {/each}
+<!-- 
+
+<script>
+	import fetchGraphQL from "$api/graphql";
+  
+	let greetings = [];
+  
+	async function fetchGreetings() {
+	  const query = `
+		query GetGreetings {
+		  getGreetings {
+			id
+			name
+			message
+		  }
+		}
+	  `;
+  
+	  try {
+		const { data } = await fetchGraphQL(query);
+		greetings = data.getGreetings;
+	  } catch (error) {
+		console.error('Error fetching greetings:', error);
+		greetings = [];
+	  }
+	}
+  
+	// Appel de la fonction au chargement du composant
+	fetchGreetings();
+  </script>
+  
+  {#if greetings.length > 0}
+	<ul>
+	  {#each greetings as greeting}
+		<li>
+		  <b>{greeting.name}</b>: {greeting.message} (ID: {greeting.id})
+		</li>
+	  {/each}
+	</ul>
+  {:else}
+	<p>No greetings found or loading...</p>
+  {/if}
+   -->
