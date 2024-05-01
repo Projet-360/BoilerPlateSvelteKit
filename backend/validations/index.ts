ad0047eb752ef3f18d20059e1d884306f2c465dc
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-05-01 03:17:54 +0200

    clean log

--- backend/validations/index.ts
+++ backend/validations/index.ts
@@ -12,7 +12,7 @@ export function graphqlValidationMiddleware(
   next: NextFunction,
 ) {
   const operationName = req.body.operationName;
-  console.log('Received variables:', req.body.variables);
+  // console.log('Received variables:', req.body.variables);
 
   let validators;
   switch (operationName) {
@@ -22,12 +22,12 @@ export function graphqlValidationMiddleware(
     default:
       return next();
   }
-  //avant
+
   Promise.all(validators.map((validation) => validation.run(req))).then(() => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      console.log('Validation errors:', errors.array());
-      console.log('Received body for error logging:', req.body);
+      // console.log('Validation errors:', errors.array());
+      // console.log('Received body for error logging:', req.body);
       res.status(400).json({ errors: errors.array() });
     } else {
       next();
