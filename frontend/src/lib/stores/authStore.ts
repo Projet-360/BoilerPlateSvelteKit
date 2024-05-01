08f17d87105b7d95aebd08677d3bf76e587ab79f
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-05-01 18:21:41 +0200

    clear code store Auth

--- frontend/src/lib/stores/authStore.ts
+++ frontend/src/lib/stores/authStore.ts
@@ -1,41 +1,7 @@
-// import { writable } from 'svelte/store';
-// import type { Writable } from 'svelte/store';
-
-// // Initialisation du store avec un état initial et un type
-// export const authStore: Writable<App.IAuthStore> = writable({
-// 	userId: null,
-// 	role: null,
-// 	isAuthenticated: false,
-// 	currentSessionId: undefined, // Initialisez à undefined ou null
-// 	sessions: [] // Initialisez avec un tableau vide
-// });
-
-// // Fonction pour initialiser le store
-// export function initializeAuthStore() {
-// 	let unsubscribe: (() => void) | null = null;
-// 	return {
-// 		promise: new Promise<void>((resolve) => {
-// 			unsubscribe = authStore.subscribe(({ isAuthenticated }) => {
-// 				if (isAuthenticated !== null) {
-// 					resolve();
-// 					if (unsubscribe) {
-// 						unsubscribe();
-// 					}
-// 				}
-// 			});
-// 		}),
-// 		unsubscribe: () => {
-// 			if (unsubscribe) {
-// 				unsubscribe();
-// 			}
-// 		}
-// 	};
-// }
-
-
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
-import client from '@apollo/client';
+import client from '$apollo';
+import { LOGIN, LOGOUT } from '$apollo/User';
 
 function createAuthStore() {
     const { subscribe, set }: Writable<App.IAuthStore> = writable({
@@ -48,42 +14,27 @@ function createAuthStore() {
 
     return {
         subscribe,
-        login: async (username, password) => {
+        login: async (username: string, password: string) => {
             const { data } = await client.mutate({
-                mutation: gql`
-                    mutation Login($username: String!, $password: String!) {
-                        login(username: $username, password: $password) {
-                            user {
-                                id
-                                username
-                                email
-                                role
-                            }
-                            token
-                        }
-                    }
-                `,
+                mutation: LOGIN,
                 variables: { username, password }
             });
             if (data.login.token) {
                 localStorage.setItem('authToken', data.login.token);
+                set({ user: data.login.user, isAuthenticated: true, token: data.login.token });
             }
-            set({ ...data.login, isAuthenticated: true });
         },
         logout: async () => {
             await client.mutate({
-                mutation: gql`
-                    mutation {
-                        logout
-                    }
-                `
+                mutation: LOGOUT
             });
             localStorage.removeItem('authToken');
-            set({ user: null, isAuthenticated: false, sessions: [] });
+            set({ user: null, isAuthenticated: false, token: null });
         },
-        // Plus de méthodes pour récupérer les données de l'utilisateur, etc.
+        // Plus de méthodes pour manipuler le state
     };
 }
 
 export const authStore = createAuthStore();
+
 
