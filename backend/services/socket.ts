f57a3cd4e739db11a931e2ab47f7210ed6567499
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-05-06 17:27:40 +0200

    clear soket emit

--- backend/services/socket.ts
+++ backend/services/socket.ts
@@ -1,30 +1,11 @@
 import { Server as IOServer } from 'socket.io';
 import { Server as HttpsServer } from 'https';
-import logger from './logger.js';
 import corsConfig from '../config/corsConfig.js';
 
 export default function initSocket(server: HttpsServer) {
   const io = new IOServer(server, {
     cors: corsConfig,
     transports: ['polling', 'websocket'],
-  });
-
-  io.on('connection', (socket) => {
-    //logger.info(`New client connected: ${socket.id}`);
-
-    socket.on('saveGreetingSocket', () => {
-      //logger.info(`Greeting sent from client: ${socket.id}`);
-      io.emit('updateGreetings');
-    });
-
-    socket.on('deleteGreetingSocket', () => {
-      //logger.info(`Greeting deleted from client: ${socket.id}`);
-      io.emit('updateGreetings');
-    });
-
-    socket.on('disconnect', () => {
-      logger.info(`Client disconnected: ${socket.id}`);
-    });
   });
 
   return io;
