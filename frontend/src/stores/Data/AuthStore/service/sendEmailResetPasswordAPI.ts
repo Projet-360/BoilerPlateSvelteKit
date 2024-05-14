845bf2f0837d531b9e038270347de8f8621d236d
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-05-14 19:51:14 +0200

    wip tests

--- frontend/src/stores/Data/AuthStore/service/sendEmailResetPasswordAPI.ts
+++ frontend/src/stores/Data/AuthStore/service/sendEmailResetPasswordAPI.ts
@@ -1,26 +1,31 @@
-import client from "$apollo";
-import { sendEmailResetPasswordGQL } from "$apollo/Auth/sendEmailResetPasswordGQL";
-import { goto } from "$app/navigation";
-import notificationStore from "$stores/UX/notificationStore";
+import client from '$apollo'
+import { sendEmailResetPasswordGQL } from '$apollo/Auth/sendEmailResetPasswordGQL'
+import { goto } from '$app/navigation'
+import notificationStore from '$stores/UX/notificationStore'
 
-async function sendEmailResetPasswordAPI(email: string, $t: TS.TranslationFunction) {   
-    try {     
-        const { data } = await client.mutate({
-            mutation: sendEmailResetPasswordGQL,
-            variables: { email}
-        });
-        if (data.sendEmailResetPassword.message === 'USER_NOT_FOUND') {
-            notificationStore.addNotification($t('data.USER_NOT_FOUND'), 'error');
-        } else if(data.sendEmailResetPassword.message === 'EMAIL_NOT_VERIFIED') {
-            notificationStore.addNotification($t('data.EMAIL_NOT_VERIFIED'), 'error');
-        } else {
-
-            notificationStore.addNotification($t('data.successSendEmailResetPassword'), 'success');
-        }
-        goto('/');
-    } catch (error) {
-        console.error('Error checking authentication:', error);
+async function sendEmailResetPasswordAPI(
+  email: string,
+  $t: TS.TranslationFunction,
+) {
+  try {
+    const { data } = await client.mutate({
+      mutation: sendEmailResetPasswordGQL,
+      variables: { email },
+    })
+    if (data.sendEmailResetPassword.message === 'USER_NOT_FOUND') {
+      notificationStore.addNotification($t('data.USER_NOT_FOUND'), 'error')
+    } else if (data.sendEmailResetPassword.message === 'EMAIL_NOT_VERIFIED') {
+      notificationStore.addNotification($t('data.EMAIL_NOT_VERIFIED'), 'error')
+    } else {
+      notificationStore.addNotification(
+        $t('data.successSendEmailResetPassword'),
+        'success',
+      )
     }
+    goto('/')
+  } catch (error) {
+    console.error('Error checking authentication:', error)
+  }
 }
 
 export default sendEmailResetPasswordAPI
--- frontend/src/stores/Data/AuthStore/service/verifyTokenAPI.ts
+++ frontend/src/stores/Data/AuthStore/service/verifyTokenAPI.ts
@@ -1,21 +1,24 @@
-import client from "$apollo";
-import { verifyTokenGQL } from "$apollo/Auth/verifyTokenGQL";
-import { goto } from "$app/navigation";
-import notificationStore from "$stores/UX/notificationStore";
-import handleErrors from "./handleErrors";
+import client from '$apollo'
+import { verifyTokenGQL } from '$apollo/Auth/verifyTokenGQL'
+import { goto } from '$app/navigation'
+import notificationStore from '$stores/UX/notificationStore'
+import handleErrors from './handleErrors'
 
 async function verifyTokenAPI(token: string, $t: TS.TranslationFunction) {
-    try {
-        const { data } = await client.query({
-            query: verifyTokenGQL,
-            variables: { token }
-        });
-        goto('/');
-        notificationStore.addNotification($t('data.emailTokenVerifiedSuccess'), 'success');                
-    } catch (error) {
-        handleErrors(error, $t, 'verifyToken');
-        notificationStore.addNotification($t('data.emailTokenVerifiedFailed'), 'error');
-    }
+  try {
+    const { data } = await client.query({
+      query: verifyTokenGQL,
+      variables: { token },
+    })
+    goto('/')
+    notificationStore.addNotification($t('data.EMAIL_VERIFIED'), 'success')
+  } catch (error) {
+    handleErrors(error, $t, 'verifyToken')
+    notificationStore.addNotification(
+      $t('data.emailTokenVerifiedFailed'),
+      'error',
+    )
+  }
 }
 
 export default verifyTokenAPI
